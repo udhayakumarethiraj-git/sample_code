1732f48b40de0b60dd20093bb4d35c5f0482b910
udhayakumarethiraj-git <udhayakumarethiraj@gmail.com>
2025-11-06 21:52:01 +0530

    Delete scripts directory

--- scripts/start_httpd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Starting httpd service..."
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
--- scripts/stop_httpd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Stopping httpd service if running..."
-sudo systemctl stop httpd || true
-
